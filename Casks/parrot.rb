91db469f6e15c2c8ed6959901c35ebeda8c7fc67
Rishi Khare <rishiskhare@gmail.com>
2026-02-22 05:42:26 -0500

    fix: switch to ARM-only cask

--- Casks/parrot.rb
+++ Casks/parrot.rb
@@ -1,15 +1,8 @@
 cask "parrot" do
   version "2026.2.1"
 
-  on_arm do
-    sha256 "REPLACE_WITH_ARM_DMG_SHA256"
-    url "https://github.com/rishiskhare/parrot/releases/download/v#{version}/Parrot_#{version}_aarch64.dmg"
-  end
-
-  on_intel do
-    sha256 "REPLACE_WITH_INTEL_DMG_SHA256"
-    url "https://github.com/rishiskhare/parrot/releases/download/v#{version}/Parrot_#{version}_x86_64.dmg"
-  end
+  sha256 "REPLACE_WITH_ARM_DMG_SHA256"
+  url "https://github.com/rishiskhare/parrot/releases/download/v#{version}/Parrot_#{version}_aarch64.dmg"
 
   name "Parrot"
   desc "Text-to-speech desktop app powered by Kokoro"
